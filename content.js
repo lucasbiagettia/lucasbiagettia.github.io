9e570759c44e895f9f0759d67cb68d4608f8ff55
lucasbiagettiaWin <lucasbiagetti@gmail.com>
2024-01-15 17:06:29 -0300

    m

--- content.js
+++ content.js
@@ -1,82 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     var headerH1 = document.querySelector("#header h1");
-//     if (headerH1) headerH1.textContent = "Lucas Biagetti";
-
-//     var headerP = document.querySelector("#header p");
-//     if (headerP) headerP.textContent = "IA Developer | Cientista de la educación";
-
-//     var navElement = document.getElementById("nav");
-//     if (navElement) {
-//         var links = navElement.getElementsByTagName("a");
-//         if (links.length >= 3) {
-//             links[0].textContent = "Presentación";
-//             links[1].textContent = "Proyectos";
-//             links[2].textContent = "Contacto";
-//         }
-//     }
-
-//     var introSection = document.getElementById("intro");
-//     if (introSection) {
-//         document.querySelector("#intro h2").textContent = "Nuevo Título de Introducción";
-//         document.querySelector("#intro p").textContent = "Nuevo texto para la introducción.";
-
-//         var learnMoreLink = introSection.querySelector(".actions a.button");
-//         if (learnMoreLink) learnMoreLink.textContent = "Mi Curriculum";
-//     }
-
-
-//     const projectsData = [
-//         {
-//           title: "Proyecto 1",
-//           iconClass: "fa-code",
-//           description: "Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat."
-//         },
-//         {
-//           title: "Proyecto 2",
-//           iconClass: "fa-copy",
-//           description: "Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat."
-//         },
-//         // Agrega más proyectos según sea necesario
-//       ];
-      
-//       function generateProjectHTML(project) {
-//         return `
-//           <li>
-//             <span class="icon solid major style1 fa-${project.iconClass}"></span>
-//             <h3>${project.title}</h3>
-//             <p>${project.description}</p>
-//           </li>
-//         `;
-//       }
-      
-//       function loadProjects() {
-//         const projectsList = document.querySelector(".features");
-//         projectsData.forEach((project) => {
-//           const projectHTML = generateProjectHTML(project);
-//           projectsList.innerHTML += projectHTML;
-//         });
-//       }
-
-//     loadProjects()
-//     // var projectsSection = document.getElementById("projects");
-//     // if (projectsSection) {
-//     //     document.querySelector("#projects h2").textContent = "Nuevo Título de Proyectos";
-
-//     //     var project1 = document.querySelector("#projects li:nth-child(1)");
-//     //     if (project1) {
-//     //         project1.querySelector("h3").textContent = "Nuevo Título del Proyecto 1";
-//     //         project1.querySelector("p").textContent = "Nuevo texto para el Proyecto 1.";
-//     //     }
-
-//     //     var project2 = document.querySelector("#projects li:nth-child(2)");
-//     //     if (project2) {
-//     //         project2.querySelector("h3").textContent = "Nuevo Título del Proyecto 2";
-//     //         project2.querySelector("p").textContent = "Nuevo texto para el Proyecto 2.";
-//     //     }
-//     // }
-// });
-
-
 document.addEventListener("DOMContentLoaded", function () {
     var headerH1 = document.querySelector("#header h1");
     if (headerH1) headerH1.textContent = "Lucas Biagetti";
@@ -96,8 +17,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var introSection = document.getElementById("intro");
     if (introSection) {
-        document.querySelector("#intro h2").textContent = "Nuevo Título de Introducción";
-        document.querySelector("#intro p").textContent = "Nuevo texto para la introducción.";
+        document.querySelector("#intro h2").textContent = "Sobre mí";
+        document.querySelector("#intro p").textContent = `Cuando era chico, me encantaban las matemáticas, mi materia favorita en la escuela. Siempre buscaba descubrir las regularidades matemáticas en el mundo.\n
+        Durante mi adolescencia, mientras participaba de distintas competencias científicas, descubrí que mi verdadera pasión era el pensamiento lógico y analítico.\n
+        Al terminar la secundaria empecé a formar estudiantes para participar de las olimpíadas matemáticas, pero honestamente estaba algo desilusionado con el entorno pedagógico que me ofrecieron, por eso decidí estudiar Ciencias de la Educación, primero en Argentina y luego en Italia.\n
+        Al regresar de Italia después de completar mi maestría, retomé esas antiguas pasiones y noté que mi nuevo trasfondo en Ciencias Sociales es especialmente útil para potenciar los desarrollos en Inteligencia Artificial.\n
+        Actualmente, me dedico profesionalmente a desarrollar software con Inteligencia Artificial y estudio cada día para ser un experto en el tema.`;
 
         var learnMoreLink = introSection.querySelector(".actions a.button");
         if (learnMoreLink) learnMoreLink.textContent = "Mi Curriculum";
@@ -106,15 +31,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const projectsData = [
         {
           title: "Proyectoasdas 1",
-          iconClass: "icon solid major style1 fa-code",
+          iconClass: "icon solid major style1",
           description: "Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat."
         },
         {
           title: "Proyecto 2",
-          iconClass: "icon major style3 fa-copy",
+          iconClass: "icon major style3",
           description: "Sed lorem amet ipsum dolor et amet nullam consequat a feugiat consequat tempus veroeros sed consequat."
         },
-        // Agrega más proyectos según sea necesario
     ];
 
     function generateProjectHTML(project) {
@@ -126,6 +50,7 @@ document.addEventListener("DOMContentLoaded", function () {
           </li>
         `;
     }
+    
 
     function loadProjects() {
         const projectsList = document.querySelector(".features");
